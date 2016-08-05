b33f43fc336e08eb7ea59b88a8a2574019643c6e
zee-german <franz.lutzenberger@gmail.com>
2016-08-05 16:26:57 +0200

    projects added

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,8 @@ $(function () {
   });
 
   try {
-    "repeat"in String.prototype && window.console.log("\n%cHi!\n\n%cI am for hire – I do more than copy & paste from stackoverflow.\n\n%chttp://www.github.com/zee-german%c \n\n", "font-family: Georgia, serif; font-size: 32px; color: #0074d9", "font-family: Georgia, serif; font-size: 16px; color: #111111", "font-family: Helvetica Neue, sans-serif; font-size: 16px; text-decoration: none; line-height: 1.2rem; color: #0074d9", "")
+    "repeat" in String.prototype && window.console.log("\n%cHi!\n\n%cI am for hire – I do more than copy & paste from stackoverflow.\n\n%chttp://www.github.com/zee-german%c \n\n", "font-family: Georgia, serif; font-size: 32px; color: #0074d9", "font-family: Georgia, serif; font-size: 16px; color: #111111", "font-family: Helvetica Neue, sans-serif; font-size: 16px; text-decoration: none; line-height: 1.2rem; color: #0074d9", "")
   } catch (E) {}
+
 
 });
